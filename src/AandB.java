9713d32c304fe94e851f6d4d063a1376854088dd
peryteil <peryteil@gmail.com>
2025-02-13 20:28:13 +0900

    a와b 출력하기

---
+++ src/AandB.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AandB {
+    public static void main(String[] ages) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
